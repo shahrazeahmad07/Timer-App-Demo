dde4466a3f3d9f5386b3750e9713b102d89ce857
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-04-09 06:12:08 +0500

    timer completed, progress bar completed

--- app/src/main/java/com/example/timerdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/timerdemoapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.timerdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import com.example.timerdemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val totalTime: Long = 60000
+    private var timePassed: Long = 0
+    private var countDownTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvTimer.text = (totalTime/1000).toString()
+        binding.progressBar.max = (totalTime/1000).toInt()
+        binding.progressBar.progress = 100
+
+        binding.btnStart.setOnClickListener{
+            startTimer()
+        }
+    }
+
+    private fun startTimer() {
+        if(countDownTimer == null) {
+            binding.progressBar.progress = (timePassed).toInt()
+            countDownTimer = object : CountDownTimer(totalTime - timePassed, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timePassed++
+                    binding.tvTimer.text = (millisUntilFinished/1000).toString()
+                    binding.progressBar.progress = (millisUntilFinished/1000).toInt()
+                }
+
+                override fun onFinish() {
+                    Toast.makeText(this@MainActivity, "Time Ended", Toast.LENGTH_SHORT).show()
+                }
+            }.start()
+        }
     }
 }
