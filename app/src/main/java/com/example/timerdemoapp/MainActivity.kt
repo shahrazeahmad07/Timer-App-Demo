dddb7db0459bfb1680015a56083cd9c5253b9070
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-04-11 01:35:36 +0500

    overriden onDestroy method

--- app/src/main/java/com/example/timerdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/timerdemoapp/MainActivity.kt
@@ -7,32 +7,32 @@ import android.widget.Toast
 import com.example.timerdemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
     private val totalTime: Long = 15000
     private var timePassed: Long = 0
     private var countDownTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        binding.tvTimer.text = (totalTime/1000).toString()
-        binding.progressBar.max = (totalTime/1000).toInt()
-        binding.progressBar.progress = binding.progressBar.max
+        binding?.tvTimer?.text = (totalTime/1000).toString()
+        binding?.progressBar?.max = (totalTime/1000).toInt()
+        binding?.progressBar?.progress = binding?.progressBar?.max!!
 
-        binding.btnStart.setOnClickListener{
+        binding?.btnStart?.setOnClickListener{
             startTimer()
         }
 
-        binding.btnPause.setOnClickListener {
+        binding?.btnPause?.setOnClickListener {
             pauseTimer()
         }
 
-        binding.btnStop.setOnClickListener {
+        binding?.btnStop?.setOnClickListener {
             resetTimer()
         }
 
-        binding.frameLayout.setOnClickListener {
+        binding?.frameLayout?.setOnClickListener {
             Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
         }
     }
@@ -42,8 +42,8 @@ class MainActivity : AppCompatActivity() {
             countDownTimer = object : CountDownTimer(totalTime - timePassed*1000, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
                     timePassed++
-                    binding.tvTimer.text = (millisUntilFinished/1000).toString()
-                    binding.progressBar.progress = (millisUntilFinished/1000).toInt()
+                    binding?.tvTimer?.text = (millisUntilFinished/1000).toString()
+                    binding?.progressBar?.progress = (millisUntilFinished/1000).toInt()
                 }
 
                 override fun onFinish() {
@@ -63,8 +63,18 @@ class MainActivity : AppCompatActivity() {
     private fun resetTimer() {
         countDownTimer?.cancel()
         countDownTimer = null
-        binding.tvTimer.text = (totalTime/1000).toString()
-        binding.progressBar.progress = binding.progressBar.max
+        binding?.tvTimer?.text = (totalTime/1000).toString()
+        binding?.progressBar?.progress = binding?.progressBar?.max!!
         timePassed = 0
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (countDownTimer != null) {
+            countDownTimer = null
+        }
+        if (binding != null) {
+            binding = null
+        }
     }
 }
