17f73637fe3609169e97431fd65df51dbdfc21cb
Shahzaib Ahmad <shahrazeahmad07@gmail.com>
2023-04-11 14:48:53 +0500

    Select time dialog added

--- app/src/main/java/com/example/timerdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/timerdemoapp/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.timerdemoapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.timerdemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
-    private val totalTime: Long = 15000
+    private var totalTime: Long = 15000
     private var timePassed: Long = 0
     private var countDownTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding?.frameLayout?.setOnClickListener {
-            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+            showSetTimerDialog()
         }
     }
 
@@ -48,6 +51,9 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onFinish() {
                     Toast.makeText(this@MainActivity, "Time Ended", Toast.LENGTH_SHORT).show()
+                    countDownTimer?.cancel()
+                    countDownTimer = null
+                    timePassed = 0
                 }
             }.start()
         }
@@ -66,6 +72,27 @@ class MainActivity : AppCompatActivity() {
         binding?.tvTimer?.text = (totalTime/1000).toString()
         binding?.progressBar?.progress = binding?.progressBar?.max!!
         timePassed = 0
+    }
+
+    private fun showSetTimerDialog() {
+        if (countDownTimer == null) {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_set_timer)
+            val btnCancel = dialog.findViewById<Button>(R.id.btnCancel)
+            btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            val btnOkay = dialog.findViewById<Button>(R.id.btnOkay)
+            btnOkay.setOnClickListener {
+                val etSetTimer = dialog.findViewById<EditText>(R.id.etSetTimer)
+                totalTime = (etSetTimer.text.toString().toLong()) * 1000
+                binding?.tvTimer?.text = (totalTime / 1000).toString()
+                binding?.progressBar?.max = (totalTime / 1000).toInt()
+                binding?.progressBar?.progress = binding?.progressBar?.max!!
+                dialog.dismiss()
+            }
+            dialog.show()
+        }
     }
 
     override fun onDestroy() {
